a78252f4bd8c7c03e1b431e6b899ee06a9374f16
Isn0v <isnovikov0@gmail.com>
2023-10-21 14:52:48 +0700

    disketload, bootload fixes

--- scripts/disketload.sh
+++ scripts/disketload.sh
@@ -1,3 +1,7 @@
-nasm -fbin boot.asm -o boot.bin;dd if=/dev/zero of=boot.img bs=1024 count=1440;
+nasm -fbin boot.asm -o boot.bin;
+gcc -m32 -ffreestanding -c -o kernel.o %SRC%/kernel.c;
+ld -m i386pe -o kernel.tmp -Ttext 0x20200 kernel.o;
+objcopy -I pe-i386 -O binary kernel.tmp kernel.bin;
+dd if=/dev/zero of=boot.img bs=1024 count=1440;
 dd if=boot.bin of=boot.img conv=notrunc;
-dd if=../bin/mess.bin of=boot.img conv=notrunc seek=1
+dd if=kernel.bin of=boot.img conv=notrunc seek=1
