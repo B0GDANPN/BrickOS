08fc8ebb3157a4e116bc62edb3f1d9188609217a
bogdan <b.pyanzin@mail.ru>
2023-10-21 12:32:21 +0700

    start creating gdt

--- scripts/boot.asm
+++ scripts/boot.asm
@@ -4,6 +4,29 @@ cli ; stack settings
 	xor ax, ax ; getting 0
 	mov ss, ax ; ss = 0
 	mov sp, 0x7C00 ; setting stack to right position
+
+gdt_start:
+    dq 0x0 ; null descriptor
+gdt_code:
+    db ...
+gdt_data:
+    db ...
+gdt_end:
+
+gdt_descriptor:
+    dw gdt_end - gdt_start - 1
+    dd gdt_start + 0x20000 ; linear address in copy vbr
+
+CODE_SEG equ gdt_code - gdt_start
+DATA_SEG equ gdt_data - gdt_start
+
+lgdt [gdt_descriptor]
+
+gdt_descriptor:
+    dw gdt_end - gdt_start - 1
+    dd gdt_start + 0x20000
+
+
 sti
 
 
